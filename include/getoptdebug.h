e995155034c8c165034f3ababfb18882ee707bad
Erich Gubler <erichdongubler@gmail.com>
2016-05-20 14:17:46 -0600

    Put debug functions into the GetOpt namespace

--- include/getoptdebug.h
+++ include/getoptdebug.h
@@ -9,95 +9,96 @@
 #include "getopt.h"
 
 #include <iostream>
-
-void printGetOptHelper(std::ostream& os){}
-void printGetOptHelper(std::ostream& os, const getopt::config& c)
+namespace GetOpt
 {
-	os << '<';
-	switch(c)
+	void printGetOptHelper(std::ostream& os){}
+	void printGetOptHelper(std::ostream& os, const GetOpt::config& c)
 	{
-		case getopt::config::caseInsensitive:
-			os << "caseInsensitive";
-			break;		
-		case getopt::config::caseSensitive:
-			os << "caseSensitive";
-			break;		
-		case getopt::config::passThrough:
-			os << "passThrough";
-			break;
-		case getopt::config::noPassThrough:
-			os << "noPassThrough";
-			break;
-		case getopt::config::bundling:
-			os << "bundling";
-			break;
-		case getopt::config::noBundling:
-			os << "noBundling";
-			break;
-		case getopt::config::required:
-			os << "required";
-			break;		
-		case getopt::config::keepEndOfOptions:
-			os << "keepEndOfOptions";
-			break;		
-		case getopt::config::stopOnFirstNonOption:
-			os << "stopOnFirstNonOption";
-			break;					
-		default:
-			os << "UNKNOWN_CONFIG_OPT";
-			break;
+		os << '<';
+		switch(c)
+		{
+			case GetOpt::config::caseInsensitive:
+				os << "caseInsensitive";
+				break;		
+			case GetOpt::config::caseSensitive:
+				os << "caseSensitive";
+				break;		
+			case GetOpt::config::passThrough:
+				os << "passThrough";
+				break;
+			case GetOpt::config::noPassThrough:
+				os << "noPassThrough";
+				break;
+			case GetOpt::config::bundling:
+				os << "bundling";
+				break;
+			case GetOpt::config::noBundling:
+				os << "noBundling";
+				break;
+			case GetOpt::config::required:
+				os << "required";
+				break;		
+			case GetOpt::config::keepEndOfOptions:
+				os << "keepEndOfOptions";
+				break;		
+			case GetOpt::config::stopOnFirstNonOption:
+				os << "stopOnFirstNonOption";
+				break;					
+			default:
+				os << "UNKNOWN_CONFIG_OPT";
+				break;
+		}
+		os << ">; ";
 	}
-	os << ">; ";
-}
 
-template<typename T, typename...Args>
-void printGetOptHelper(std::ostream& os, const std::string& s, const T& t, Args&&...args)
-{
-	os << "\"" << s << "\": " << t << "; ";
-	printGetOptHelper(os, args...);
-}
+	template<typename T, typename...Args>
+	void printGetOptHelper(std::ostream& os, const std::string& s, const T& t, Args&&...args)
+	{
+		os << "\"" << s << "\": " << t << "; ";
+		printGetOptHelper(os, args...);
+	}
 
-template<typename T, typename...Args>
-void printGetOptHelper(std::ostream& os, const char* c, const T& t, Args&&...args)
-{
-	printGetOptHelper(os, std::string(c), t, args...);
-}
+	template<typename T, typename...Args>
+	void printGetOptHelper(std::ostream& os, const char* c, const T& t, Args&&...args)
+	{
+		printGetOptHelper(os, std::string(c), t, args...);
+	}
 
-template<typename...Args>
-void printGetOptHelper(std::ostream& os, const getopt::config& c, Args&&...args)
-{
-	printGetOptHelper(os, c);
-	printGetOptHelper(os, args...);
-}
+	template<typename...Args>
+	void printGetOptHelper(std::ostream& os, const GetOpt::config& c, Args&&...args)
+	{
+		printGetOptHelper(os, c);
+		printGetOptHelper(os, args...);
+	}
 
-template<typename...Args>
-void printGetOptArgs(std::ostream& os, const char* prepend, Args&&...args)
-{
-	os << prepend << ": { ";
-	printGetOptHelper(os, args...);
-	os << "}" << std::endl;
-}
+	template<typename...Args>
+	void printGetOptArgs(std::ostream& os, const char* prepend, Args&&...args)
+	{
+		os << prepend << ": { ";
+		printGetOptHelper(os, args...);
+		os << "}" << std::endl;
+	}
 
-template<typename...Args>
-void printGetOptArgs(std::ostream& os, const std::string& prepend, Args&&...args)
-{
-	os << prepend << ": { ";
-	printGetOptHelper(os, args...);
-	os << "}" << std::endl;
-}
+	template<typename...Args>
+	void printGetOptArgs(std::ostream& os, const std::string& prepend, Args&&...args)
+	{
+		os << prepend << ": { ";
+		printGetOptHelper(os, args...);
+		os << "}" << std::endl;
+	}
 
-//REMOVE: Debug functions
-void variadicAddresses(){}
-template<typename...Ts>
-void variadicAddresses(getopt::config& c, Ts&&...ts)
-{
-	std::cout << "<config>; ";
+	//REMOVE: Debug functions
+	void variadicAddresses(){}
+	template<typename...Ts>
+	void variadicAddresses(GetOpt::config& c, Ts&&...ts)
+	{
+		std::cout << "<config>; ";
+	}
+	template<typename T, typename...Ts>
+	void variadicAddresses(T& t, Ts&&...ts)
+	{
+		std::cout << typeid(T).name() << ": " << t << ", address = " << &t << "; ";
+		variadicAddresses(ts...);
+	}
 }
-template<typename T, typename...Ts>
-void variadicAddresses(T& t, Ts&&...ts)
-{
-	std::cout << typeid(T).name() << ": " << t << ", address = " << &t << "; ";
-	variadicAddresses(ts...);
-}
-
 #endif
