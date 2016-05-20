9b071a6cea4fc1a87cc6fe2dad226a426128b875
Erich Gubler <erichdongubler@gmail.com>
2016-05-20 14:16:02 -0600

    Clean up and clarify `basic.cpp` comments

--- example/basic.cpp
+++ example/basic.cpp
@@ -32,49 +32,57 @@ int main(int argc, char** argv)
 	{
 		results = GetOpt::getopt(argc, argv
 
-		// Define short opt "l" and long opt "length" for int length. Note the following:
-			// * accepts args of the forms:
+		// Define short opt "l" and long opt "length" for the int variable
+		// "length". Note the following:
+			// * GetOpt will parse the value of the option if it finds these:
 			//   * ["-l24"]
 			//   * ["-l", "24"]
 			//   * ["--length=24"]
 			//   * ["--length", "24"]
-			// * opt specifications are delimited by '|'
-			// * The value given in args must be parseable to an int, obviously!
-			// * Repeat definitions of an opt will override the previous value, so ["-l", "20", "-l300"] would result in length == 300.
+			// * opt specifications are delimited by '|'.
+			// * The value given in args must be parseable to an int,
+			//   obviously! Otherwise, an exception is thrown.
+			// * Repeat definitions of an opt will override the previous value,
+			//   so ["-l", "20", "-l300"] would result in length == 300.
 		, "l|length", &length
 
 		// Define short opt "f" and long opt "file" for string data
-		, "file|f", &data
+		, "file|f", &data // You don't have to have the same variable names!
 
-		// Uncomment this config value to allow unrecognized opts on the command line starting from this point.
-		// config::passThrough is useful for creating modular getopt calls.
-
+		// Uncomment the config value below to allow unrecognized opts on the
+		// command line starting from this point. config::passThrough is
+		// useful for creating modular getopt calls.
+		// 
 		/*, GetOpt::config::passThrough*/
+		//
+		// Notice: if I pass config::passThrough in here, previous unrecognized
+		// options will still throw an exception. If passThrough were given
+		// here, GetOpt would still expect the "length" and "file" options to
+		// come before any unrecognized args.
 
-		// Notice: if I pass config::passThrough in here, unrecognized args found previous to it will still
-		//     cause getopt to throw an exception. If I passed in passThrough here, it would still expect
-		//     length and file to come before any unrecognized args.
-
-		// Define incremental opt for verbosity; long is "verbose", short is "v"
-		// Integral types can be used as incremental opts, which count the number of occurences of the flag a la bool.
-		// For instance, "-v --verbose -v" at the command line would set verbosity to 3.
+		// Define incremental opt for verbosity; long is "verbose", short is
+		// "v". Integral types can be used as incremental opts, which count the
+		// number of occurences of the flag a la bool.
+		// 
+		// For instance, "-v --verbose -v" at the command line would set
+		// verbosity to 3.
 		, "verbose|v+", &verbosity 
 
 		// Define short opt "c" and two long opts "color" and "colour" for string color
 		, "color|c|colour", &color
 
-		// This is like the others, except I *won't* need to provide a value normally.
-		// Bools are accepted of the following forms:
+		// For booleans, GetOpt only expects a single arg for them.
+		// Boolean args are parsed by GetOpt as expected when it finds the following:
 		// * ["--pretty"]
 		// * ["-p"]
 		// * ["-ptrue"]
 		// * ["-pfalse"]
 		// * ["--pretty=true"]
 		// * ["--pretty=false"]
-
-		// However, the following forms skip the second arg:
+		// 
+		// However, GetOpt skips the second arg if you try to provide one:
 		// * ["--pretty", "true"]
-		// * ["-p", "true"]
+		// * ["-p", "false"]
 		, "pretty|p", &usePrettyOutput);
 	}
 	catch(GetOpt::GetOptException e)
@@ -95,7 +103,6 @@ int main(int argc, char** argv)
 		defaultGetoptPrinter("Here's valid args for this program:", results.result.options);
 
 	// Print remaining args (which were removed during args parsing for convenience)
-	// Comment out this line and the cpputils include if you want to quickly 
 	cout << "Args remaining: ";
 	auto begin = results.args.begin();
 	auto end = results.args.end();
